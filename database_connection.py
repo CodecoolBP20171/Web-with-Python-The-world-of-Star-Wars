5c8cb7fb6f2cd068d5e64521a4e79d5032bec460
Annamaria Sojnoczki <soj.annam@gmail.com>
2017-07-03 20:30:47 +0200

    deploying process

--- database_connection.py
+++ database_connection.py
@@ -1,11 +1,21 @@
+
+import os
 import psycopg2
+import urllib
 
 
 def db_request(query):
     """Establish connection for database request"""
     try:
-        connect_str = "dbname=potyi user=potyi host='localhost' password=joinme9"
-        conn = psycopg2.connect(connect_str)
+        urllib.parse.uses_netloc.append('postgres')
+        url = urllib.parse.urlparse(os.environ.get('DATABASE_URL'))
+        conn = psycopg2.connect(
+            database=url.path[1:],
+            user=url.username,
+            password=url.password,
+            host=url.hostname,
+            port=url.port
+        )
         conn.autocommit = True
     except:
         print("I am unable to connect to the database")
@@ -19,8 +29,15 @@ def db_request(query):
 def db_update(query):
     """Establish connection for database update"""
     try:
-        connect_str = "dbname=potyi user=potyi host='localhost' password=joinme9"
-        conn = psycopg2.connect(connect_str)
+        urllib.parse.uses_netloc.append('postgres')
+        url = urllib.parse.urlparse(os.environ.get('DATABASE_URL'))
+        conn = psycopg2.connect(
+            database=url.path[1:],
+            user=url.username,
+            password=url.password,
+            host=url.hostname,
+            port=url.port
+        )
         conn.autocommit = True
     except:
         print("I am unable to connect to the database")
