2838ca6ebe03333ef8d114aa6d2dd3a88de49b19
Annamaria Sojnoczki <soj.annam@gmail.com>
2017-07-03 21:37:32 +0200

    ref5

--- static/planets.js
+++ static/planets.js
@@ -2,20 +2,6 @@ var app = app || {};
 
 app.init = function () {
     var pageCount = 1;
-    function pageHandler(pageCount) {
-        $(document).on('click','#next', function(){
-        pageCount++;
-        app.dataHandler.getPlanetsData(pageCount);
-        });
-        $(document).on('click','#previous', function(){
-            if (pageCount === 0) {
-                pageCount = 1;
-                app.dataHandler.getPlanetsData(pageCount);
-            } else {
-                pageCount--;
-                app.dataHandler.getPlanetsData(pageCount);
-            };       
-        });
     app.events.pageHandlers(pageCount);
     app.dataHandler.getPlanetsData(pageCount);
     app.events.eventHandler();
