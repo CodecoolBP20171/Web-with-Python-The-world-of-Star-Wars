d42b37940ab07512f88ab3599eef5e41f1156b15
Annamaria Sojnoczki <soj.annam@gmail.com>
2017-07-03 21:47:59 +0200

    remove data

--- static/planets.js
+++ static/planets.js
@@ -1,6 +1,20 @@
 var app = app || {};
 
 var pageCount = 1;
+pageHandlers: function (pageCount) {
+        $(document).on('click','#next', function(){
+        pageCount++;
+        app.dataHandler.getPlanetsData(pageCount);
+        });
+        $(document).on('click','#previous', function(){
+            if (pageCount === 0) {
+                pageCount = 1;
+                app.dataHandler.getPlanetsData(pageCount);
+            } else {
+                pageCount--;
+                app.dataHandler.getPlanetsData(pageCount);
+            };       
+        });
 app.events.pageHandlers(pageCount);
 app.dataHandler.getPlanetsData(pageCount);
 app.events.eventHandler();
