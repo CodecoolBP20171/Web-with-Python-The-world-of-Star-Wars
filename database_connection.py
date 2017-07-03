454ee81739158301e37b4ee247dfcba31f22ac59
Annamaria Sojnoczki <soj.annam@gmail.com>
2017-07-03 21:01:59 +0200

    refactor run.txt

--- database_connection.py
+++ database_connection.py
@@ -1,9 +1,35 @@
-
-import os
 import psycopg2
-import urllib
+
+"""
+def db_request(query):
+    """Establish connection for database request"""
+    try:
+        connect_str = "dbname=potyi user=potyi host='localhost' password=joinme9"
+        conn = psycopg2.connect(connect_str)
+        conn.autocommit = True
+    except:
+        print("I am unable to connect to the database")
+    cur = conn.cursor()
+    cur.execute(query)
+    table = cur.fetchall()
+    table = list(map(list, table))
+    return table
 
 
+def db_update(query):
+    """Establish connection for database update"""
+    try:
+        connect_str = "dbname=potyi user=potyi host='localhost' password=joinme9"
+        conn = psycopg2.connect(connect_str)
+        conn.autocommit = True
+    except:
+        print("I am unable to connect to the database")
+    cur = conn.cursor()
+    cur.execute(query)
+    conn.close()
+    return None
+
+"""
 def db_request(query):
     """Establish connection for database request"""
     try:
@@ -45,3 +71,4 @@ def db_update(query):
     cur.execute(query)
     conn.close()
     return None
+
